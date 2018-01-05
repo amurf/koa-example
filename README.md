56078d291ac1e7c604b36aa0ff89aeadc0bd271e
ash <ash@strategicdata.com.au>
2018-01-05 17:00:18 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# koa-example
+
+Simple example of how to do a small api using koa 2
+
+`docker-compose up` to run.
---
+++ src/app.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+const logger     = require('koa-logger');
+const session    = require('koa-session');
+const cors 	  	 = require('kcors');
+
+
+const config = require('./config');
+const app 	 = new Koa();
+
+const Router = require('koa-router');
+const router = new Router();
+router.get('/', (ctx, next) => { ctx.body = {hello: 'world'}; });
+
+// Set middlewares
+app
+	.use(bodyParser())
+	.use(logger())
+	.use(cors(config.cors))
+	.use(session(config.session, app))
+	.use(router.routes())
+	.use(router.allowedMethods())
+;
+
+// Start server
+if (!module.parent) {
+	app.listen(config.server.port, config.server.ip, () => {
+		console.log(`API server listening on ${config.server.host}:${config.server.port}, in ${config.server.env}`);
+	});
+}
+
+// Expose app
+module.exports = app;
